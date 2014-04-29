f06f6093e9b00dcb1be1e9c0b1e4d1f1b7e5be72
Mohammed Aljebreen <aljebreen50@gmail.com>
2014-04-28 22:03:24 -0400

    Balance response

--- src/messages/BalanceResponse.java
+++ src/messages/BalanceResponse.java
@@ -17,6 +17,10 @@ public class BalanceResponse extends BankingMessage {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public double getClientBalance() {
+		return client.clientBalance;
+	}
+	
 	public boolean send(){
 		System.out.println("Not implemented:BalanceResponse::send");
 		return false;
